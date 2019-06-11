0240978d2be1553dc25ee502b0b69144430e2c21
Jeff Gonzalez <jeff@Hypertheory>
2019-06-11 10:19:05 -0400

    have the communications compoennts about ready

---
+++ src/app/components/communications/rhs/rhs.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rhs',
+  templateUrl: './rhs.component.html',
+  styleUrls: ['./rhs.component.css']
+})
+export class RhsComponent implements OnInit {
+
+  message = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getIt() {
+    // do something here.
+  }
+
+}

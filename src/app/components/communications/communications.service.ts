8e01cf27410706721f45fa010d860bdc9b5d5a4e
Jeff Gonzalez <jeff@Hypertheory>
2019-06-11 11:01:32 -0400

    bad subscription

--- src/app/components/communications/communications.service.ts
+++ src/app/components/communications/communications.service.ts
@@ -1,13 +1,20 @@
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export class CommunicationsService {
 
   private message = 'default';
 
+  private subject = new BehaviorSubject<string>('some default value');
   getMessage() {
     return this.message;
   }
 
   setMessage(newMessage: string) {
     this.message = newMessage;
+    this.subject.next(newMessage);
+  }
+
+  getSubscription(): Observable<string> {
+    return this.subject.asObservable();
   }
 }
--- src/app/components/communications/rhs/rhs.component.ts
+++ src/app/components/communications/rhs/rhs.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommunicationsService } from '../communications.service';
+import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rhs',
   templateUrl: './rhs.component.html',
   styleUrls: ['./rhs.component.css']
 })
-export class RhsComponent implements OnInit {
+export class RhsComponent implements OnInit, OnDestroy {
 
   message = '';
+  message2: string;
+  subscription: Subscription;
   constructor(private service: CommunicationsService) { }
 
   ngOnInit() {
+    this.subscription = this.service.getSubscription()
+      .subscribe(newMessage => {
+        console.log('Got a new message: ', newMessage);
+        this.message2 = newMessage;
+      });
   }
 
   getIt() {
     this.message = this.service.getMessage();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
